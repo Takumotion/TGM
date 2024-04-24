d42aba99bca8a93501422c1d3c563594cf222e8f
takumotion <office@takumotion.com>
2024-04-24 13:09:46 +0200

    fdsfdsaf

--- mechanics/projectile/main.py
+++ mechanics/projectile/main.py
@@ -2,15 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
 
-def formula(v_1, v_2, angle):
+def formula(v_1, v_2, angle_1, angle_2):
     # Convert angle to radians
-    angle_rad = np.radians(angle)
-    
+    angle_1_rad = np.radians(angle_1)
+    angle_2_rad = np.radians(angle_2)
     # Calculate the x and y components of the velocities
-    vx_1 = v_1 * np.cos(angle_rad)
-    vy_1 = v_1 * np.sin(angle_rad)
-    vx_2 = v_2 * np.cos(angle_rad)
-    vy_2 = v_2 * np.sin(angle_rad)
+    vx_1 = v_1 * np.cos(angle_1_rad)
+    vy_1 = v_1 * np.sin(angle_1_rad)
+    vx_2 = v_2 * np.cos(angle_2_rad)
+    vy_2 = v_2 * np.sin(angle_2_rad)
     
     # Update the equations with the velocity components
     x_1 = vx_1 * t
@@ -24,12 +24,13 @@ def formula(v_1, v_2, angle):
 # General variable and constant values
 t = np.linspace(0, 10, 1000)  # Adjusted for realistic time frame
 g = 9.81  # acceleration due to gravity in m/s^2
-v_1 = 50  # initial velocity in m/s
-v_2 = 1   # initial velocity in m/s
-h = 10    # initial height in meters
-angle = 20  # angle in degrees
+v_1 = 20  # initial velocity in m/s
+v_2 = 0   # initial velocity in m/s
+h = 5    # initial height in meters
+angle_1 = 30  # angle in degrees
+angle_2 = 0  # angle in degrees
 
-y_1, y_2, x_1, x_2 = formula(v_1, v_2, angle)
+y_1, y_2, x_1, x_2 = formula(v_1, v_2, angle_1, angle_2)
 
 fig, ax = plt.subplots()
 scat_1 = ax.scatter(x_1[0], y_1[0], c="b", s=5, label=f'v0 = {v_1} m/s')
